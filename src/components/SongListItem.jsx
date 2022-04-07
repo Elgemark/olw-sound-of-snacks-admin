316507d14be3c3c3e7d91b3e639e36e4c882d4ed
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-04-07 16:06:11 +0200

    Update

--- src/components/SongListItem.jsx
+++ src/components/SongListItem.jsx
@@ -6,22 +6,33 @@ const OpenIcon = styled(Open)`
   height: 20px;
 `;
 
-const SongListItem = styled(({ show = true, checkBox = true, song, index, checked, onChange, ...rest }) => (
+const SongListItem = styled(({ show = true, checkBox = true, song, index, checked, onChange, onOpen, ...rest }) => (
   <li key={song.id} index={index} {...rest}>
     {checkBox && <input type="checkbox" checked={checked} onChange={onChange} />}
     <span className="num">{`${index + 1}.`}</span>
     <span className="alias">{song.alias}</span>
     <span className="email">{song.email}</span>
-    <OpenIcon />
+    <OpenIcon className="open-icon" onClick={onOpen} />
   </li>
 ))`
-  cursor: pointer;
-  display: grid;
-  grid-template-columns: 50px 50px 150px 150px;
-  grid-template-rows: auto;
+  width: 100%;
+  display: flex;
   background-color: ${({ index, color }) => (color ? color : index % 2 ? "#fff" : "#eee")};
   &:hover {
     background-color: #ffeeff;
+  }
+  .num {
+    width: 30px;
+  }
+  .alias {
+    width: 150px;
+  }
+  .email {
+    flex-grow: 1;
+  }
+  .open-icon {
+    justify-self: flex-end;
+    cursor: pointer;
   }
 `;
 
