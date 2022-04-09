ac975c85b50767d392e38ae05fff1053f4472cc7
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-04-09 10:51:22 +0200

    Update hooc

--- src/hooks/songs.jsx
+++ src/hooks/songs.jsx
@@ -3,8 +3,9 @@ import { collection, query, orderBy, startAfter, limit as fbLimit, getDocs } fro
 import { getDb } from "../firebase";
 
 const getSongs = async ({ limit, fromSong }) => {
-  const firstQuery = query(collection(getDb(), "songs"), fbLimit(limit));
-  const documentSnapshots = await getDocs(firstQuery);
+  const defaultQuery = query(collection(getDb(), "songs"), fbLimit(limit));
+  const fromQuery = fromSong && query(collection(getDb(), "songs"), fbLimit(limit), startAfter(fromSong));
+  const documentSnapshots = await getDocs(fromQuery || defaultQuery);
   const lastSong = documentSnapshots.docs[documentSnapshots.docs.length - 1];
   const songs = documentSnapshots.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
   return { songs, lastSong };
@@ -12,11 +13,12 @@ const getSongs = async ({ limit, fromSong }) => {
 
 export const useGetSongs = ({ fromSong, limit = 20 }) => {
   const [songs, setSongs] = useState([]);
-  const [lastSong, setLastSong] = useState([]);
+  const [lastSong, setLastSong] = useState();
 
   useEffect(() => {
     getSongs({ limit, fromSong: lastSong }).then((res) => {
       setSongs(res.songs);
+      setLastSong(res.lastSong);
     });
   }, [limit, fromSong]);
 
