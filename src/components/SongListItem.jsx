5f87becfae04b3e5c60518b9497fa1ad25c9309e
Henrik Elgemark <henrik.elgemark@funkismultimedia.se>
2022-04-09 11:37:59 +0200

    Remove showing mail

--- src/components/SongListItem.jsx
+++ src/components/SongListItem.jsx
@@ -11,7 +11,6 @@ const SongListItem = styled(({ show = true, checkBox = true, song, index, checke
     {checkBox && <input type="checkbox" checked={checked} onChange={onChange} />}
     <span className="num">{`${index + 1}.`}</span>
     <span className="alias">{song.alias}</span>
-    <span className="email">{song.email}</span>
     <OpenIcon className="open-icon" onClick={onOpen} />
   </li>
 ))`
@@ -25,11 +24,9 @@ const SongListItem = styled(({ show = true, checkBox = true, song, index, checke
     width: 30px;
   }
   .alias {
-    width: 150px;
-  }
-  .email {
     flex-grow: 1;
   }
+
   .open-icon {
     justify-self: flex-end;
     cursor: pointer;
